ef08a85bc1b6d8da6f1815d033105b2bab7ebe0f
Kameron Lutes <kalutes@google.com>
2026-09-16 03:41:00 -0700

    Remove manual chrome installation from loadline end2end
    The loadline end2end test was manually installing a newer version of
    chrome since the CQ bots came with a very old version preinstalled.
    
    With the new android emulator in CQ, the preinstalled version is new
    enough already, so the manual installation was skipped. The test was
    still specifying chrome canary in the browser config, which caused the
    test to fail when it didn't find a canary browser.
    
    Remove the manual installation logic since it is no longer necessary.
    
    Change-Id: I626c8d3c8f3660881c629c3a9330ea5982cd0a53
    Reviewed-on: https://chromium-review.googlesource.com/c/crossbench/+/8414027
    Reviewed-by: Mikhail Khokhlov <khokhlov@google.com>
    Reviewed-by: Camillo Bruni <cbruni@chromium.org>

--- tests/end2end/android/loadline/test_loadline.py
+++ tests/end2end/android/loadline/test_loadline.py
@@ -6,65 +6,13 @@ from __future__ import annotations
 
 import enum
 import json
-import logging
 from typing import TYPE_CHECKING
 
-import pytest
-
-from crossbench.browsers.chrome.version import ChromeVersion
 from crossbench.cli.cli import CrossBenchCLI
-from crossbench.path import check_hash
-from crossbench.plt import PLATFORM
-from crossbench.plt.android_adb import Adb, AndroidAdbPlatform
 from tests import test_helper
 
 if TYPE_CHECKING:
   from tests.test_helper import TestEnv
-
-CHROME_APK_URL = "gs://chrome-telemetry/apks/MonochromeCanary.apk"
-CHROME_APK_HASH = "5de59881c02783d2174e1e891d82c9dbbce09c67"
-
-MIN_VERSION = ChromeVersion.any((130,))
-
-
-@pytest.fixture(autouse=True)
-def adb_test_env(device_id, adb_path, test_env: TestEnv) -> None:
-  tmp_dir = test_env.output_dir / "tmp_dir"
-  assert not tmp_dir.exists()
-
-  adb = Adb(PLATFORM, device_id, adb_path)
-  adb_platform = AndroidAdbPlatform(PLATFORM, adb=adb)
-
-  installed_version_str = adb_platform.app_version("com.android.chrome")
-  installed_version = ChromeVersion.parse(installed_version_str)
-  if installed_version >= MIN_VERSION:
-    logging.info("Using pre-installed chrome version %s", installed_version)
-  else:
-    # Download and install chrome-canary M130 (x64 arch) from the cloud.
-    # The benchmark requires trace events that were introduced in M126.
-    # TODO(crbug/377290309): Remove this workaround when chrome preinstalled
-    # in the emulator image is >=M126.
-    local_apk = tmp_dir / "chrome.apk"
-    PLATFORM.download_gcs_file(CHROME_APK_URL, local_apk)
-    assert check_hash(local_apk, CHROME_APK_HASH)
-
-    assert adb_path, "Missing adb"
-    assert device_id, "Missing device id"
-    adb = Adb(PLATFORM, device_id, adb_path)
-    adb.install_apk(local_apk)
-
-
-# TODO(crbug/377290309): Remove the custom browser config when the test passes
-# with the preinstalled browser.
-def _browser_config(device_id, adb_path) -> str:
-  return json.dumps({
-      "browser": "chrome-canary",
-      "driver": {
-          "type": "adb",
-          "device_id": device_id,
-          "adb_bin": adb_path
-      }
-  })
 
 
 def _batch_trace_process_config() -> str:
@@ -135,18 +83,17 @@ def _verify_breakdown(out_dir):
     assert has_values
 
 
-def test_loadline_phone(device_id, adb_path, test_env: TestEnv) -> None:
-  _test_loadline_default(device_id, adb_path, BenchmarkType.PHONE, test_env)
+def test_loadline_phone(browser_config, test_env: TestEnv) -> None:
+  _test_loadline_default(browser_config, BenchmarkType.PHONE, test_env)
 
 
-def test_loadline_tablet(device_id, adb_path, test_env: TestEnv) -> None:
-  _test_loadline_default(device_id, adb_path, BenchmarkType.TABLET, test_env)
+def test_loadline_tablet(browser_config, test_env: TestEnv) -> None:
+  _test_loadline_default(browser_config, BenchmarkType.TABLET, test_env)
 
 
-def _test_loadline_default(device_id, adb_path, benchmark_type: BenchmarkType,
+def _test_loadline_default(browser_config: str, benchmark_type: BenchmarkType,
                            test_env: TestEnv) -> None:
   cli = CrossBenchCLI()
-  browser_config = _browser_config(device_id, adb_path)
   out_dir = test_env.results_dir / f"default_{benchmark_type}"
   cli.run([
       benchmark_type, f"--browser={browser_config}", "--repeat=1", "--throw",
@@ -158,9 +105,8 @@ def _test_loadline_default(device_id, adb_path, benchmark_type: BenchmarkType,
   _verify_breakdown(out_dir)
 
 
-def test_loadline_batch(device_id, adb_path, test_env: TestEnv) -> None:
+def test_loadline_batch(browser_config, test_env: TestEnv) -> None:
   cli = CrossBenchCLI()
-  browser_config = _browser_config(device_id, adb_path)
   out_dir = test_env.results_dir
   # We run the benchmark with increased time units to account for
   # the slowness of emulators on test bots.
